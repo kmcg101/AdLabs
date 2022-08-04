0ef5fefdfbb879819f7433a7a529c559386b8002
Kevin McGur <kevin.mcgurn@mac.com>
2022-08-04 15:27:32 -0400

    3 second pause before screen grab 4.20

--- src/pages/Inputs.js
+++ src/pages/Inputs.js
@@ -46,7 +46,7 @@ const Inputs = (props, { inputsCheckButtonPressed, inputsCheckButtonPressedOnce,
         <div className="inputsLeftColumn">
           <TextBox
             value={props.inputValues.client}
-            label="CLIENT NAME: v4.10"
+            label="CLIENT NAME: v4.20"
             varID="client"
             handleAnyInputsChange={handleAnyInputsChange}
             inputsCheckButtonPressed={inputsCheckButtonPressed}
